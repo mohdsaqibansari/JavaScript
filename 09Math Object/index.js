46c9d9bf159dabc3680582645485f253c522889c
MOHD SAQIB ANSARI <106581457+mohdsaqibansari@users.noreply.github.com>
2022-06-08 21:03:47 +0530

    Add files via upload

---
+++ 09Math Object/index.js
@@ -0,0 +1,31 @@
+console.log(Math.PI);
+
+// Math.round(): it will return 10.2455 as 10 and 10.512 as 11
+console.log(Math.round(10.499));
+console.log(Math.round(10.5111));
+
+// Math.pow(x,y): It will return value of x raised to power of y
+console.log(Math.pow(2, 3));
+console.log(Math.pow(16, 0.5));
+
+// Math.sqrt(): It will give square root of number provided.
+console.log(Math.sqrt(100));
+
+// Math.abs()
+console.log(Math.abs(-19));
+
+// Math.ceil():It will give next Integer
+console.log(Math.ceil(10.2977));
+
+// Math.floor():It will give next previous Integer
+console.log(Math.floor(10.99999));
+
+// Math.Min()
+console.log(Math.min(1, 0, 9, 2, -1, 7));
+
+// Math.random(): It will give a number ranging from 0 to 1
+
+console.log(Math.floor(Math.random() * 10)); //It will give number from 0 to 9
+
+// Math.trunc(): It will give only Integer part of the number
+console.log(Math.trunc(-99.76));
