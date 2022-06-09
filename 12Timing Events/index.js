d32700628c2723eec95759f4cb9ea4c496342c5f
MOHD SAQIB ANSARI <106581457+mohdsaqibansari@users.noreply.github.com>
2022-06-09 18:42:00 +0530

    Add files via upload

---
+++ 12Timing Events/index.js
@@ -0,0 +1,12 @@
+/*These Time Intervals are called Timing Events
+
+There are two key methods to use with JavaScript :
+
+setTimeout(function,milliseconds):It executes a function after waiting for given
+number of milliseconds.
+
+
+setInterval(function,milliseconds):It repeats the execution of Function after 
+the number of milliseconds
+
+*/
