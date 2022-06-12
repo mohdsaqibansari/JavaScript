b868a98c7182b2d9711c44b604fc45052916c472
MOHD SAQIB ANSARI <106581457+mohdsaqibansari@users.noreply.github.com>
2022-06-12 11:06:51 +0530

    Add files via upload

---
+++ 14Event Propagation/index.js
@@ -0,0 +1,10 @@
+/*
+Event Bubbling and Capturing are two ways of Event Propagation in the HTML, DOM API, when 
+an event occurs in an element inside another element and both elements have registered a handle for
+that event.
+The event propagation determines in which order the elements receive the event.
+ It can  be divided into 3 categories
+ 1. Capture Phase: Going from Window to the Event Target Phase (Top to Bottom)
+ 2. Target Phase: It is the Target phase.
+ 3. Bubble Phase: From the event Target to the Window.(Bottom to Top)
+*/
